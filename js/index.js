aacb1fa04daa0bcaa550fd26d33a05842f9f3eb7
MargNation <margaret.w.harriman@gmail.com>
2017-01-16 15:17:04 -0500

    Personal Portfolio

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//Hello! My name is  and I live in  I am studying to be a Web Developer online at Free Code Camp. I am focused on building a solid foundation in JS, HTML, and CSS. I am also learning Git, unit testing, and responsive design. After I have completed Free Code Camp's Front End Development Certification, I plan on working through the Data Visualization and Back End sections until I finish my Full Stack Certifcation.
+
+
+var doc = new jsPDF();
+var specialElementHandlers = {
+    '#editor': function (element, renderer) {
+        return true;
+    }
+};
+
+$('#cmd').click(function () {   
+    doc.fromHTML($('#content').html(), 15, 15, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+    doc.save('sample-file.pdf');
+});
